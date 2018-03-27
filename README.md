259a8df7deebdc79579d5c4af2f078c8fff95c55
Dillies Benoît <benoit.dillies@ynov.com>
2018-03-27 18:02:20 +0200

    swap to node

--- README.md
+++
@@ -1,1 +0,0 @@
-# Elquide-calculator
--- public/javascripts/calculator.js
+++ public/javascripts/calculator.js
@@ -1,8 +1,3 @@
-var CheminComplet = document.location.href;
-var CheminRepertoire = CheminComplet.substring(0, CheminComplet.lastIndexOf("/"));
-var NomDuFichier = CheminComplet.substring(CheminComplet.lastIndexOf("/") + 1);
-console.log('NomDuFichier : \n' + NomDuFichier + ' \n\nCheminRepertoire : \n' + CheminRepertoire + ' \n\nCheminComplet : \n' + CheminComplet)
-
 var arome = document.getElementById("arome").value;
 var base = document.getElementById("base").value;
 
@@ -15,8 +10,10 @@ $("#arome").change(function () {
 });
 
 $("#base").change(function () {
-    arome = document.getElementById("arome").value;
-    base = document.getElementById("base").value;
+    if (document.getElementById("arome").value & document.getElementById("base").value) {
+        arome = document.getElementById("arome").value;
+        base = document.getElementById("base").value;
+    }
     var result = Number(arome) * Number(base) / 100;
     document.getElementById("result").value=result;
     console.log(result);
