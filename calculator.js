55a07397083366f8e56b3df8652ef5949688b941
Dillies Benoît <benoit.dillies@ynov.com>
2018-05-20 21:56:27 +0200

    intellij git integration test
    Signed-off-by: Dillies Benoît <benoit.dillies@ynov.com>

--- calculator.js
+++ calculator.js
@@ -1,20 +1,20 @@
-var arome = document.getElementById("arome").value;
-var base = document.getElementById("base").value;
+let arome = document.getElementById("arome").value;
+let base = document.getElementById("base").value;
 
 $("#arome").change(function () {
     arome = document.getElementById("arome").value;
     base = document.getElementById("base").value;
-    var result = Number(arome) * Number(base) / 100;
+    let result = Number(arome) * Number(base) / 100;
     document.getElementById("result").value=result;
     console.log(result);
 });
 
 $("#base").change(function () {
-    if (document.getElementById("arome").value & document.getElementById("base").value) {
+    if (document.getElementById("arome").value && document.getElementById("base").value) {
         arome = document.getElementById("arome").value;
         base = document.getElementById("base").value;
     }
-    var result = Number(arome) * Number(base) / 100;
+    let result = Number(arome) * Number(base) / 100;
     document.getElementById("result").value=result;
     console.log(result);
 });
